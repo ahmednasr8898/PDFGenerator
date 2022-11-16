492fbf01e8ecf893067de029ab1810bce3fb42aa
Ahmed Nasr <ahmed.nasr.saieed@gmail.com>
2022-11-16 12:53:44 +0200

    push demo

--- PDFGenerator/ViewController.swift
+++ PDFGenerator/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func saveAsPdfButtonWasTapped(_ sender: Any) {
+        let filePath = self.view.exportAsPdfFromView()
+        sharePdf(filePath: filePath)
+    }
+    
+    
+    private func sharePdf(filePath: String) {
+        let url = URL(fileURLWithPath: filePath)
+        let activityViewController: UIActivityViewController = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        let excludedActivities = [UIActivity.ActivityType.postToFlickr, UIActivity.ActivityType.postToWeibo, UIActivity.ActivityType.message, UIActivity.ActivityType.mail, UIActivity.ActivityType.print, UIActivity.ActivityType.copyToPasteboard, UIActivity.ActivityType.assignToContact, UIActivity.ActivityType.saveToCameraRoll, UIActivity.ActivityType.addToReadingList, UIActivity.ActivityType.postToFlickr, UIActivity.ActivityType.postToVimeo,UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToTencentWeibo]
+        
+        activityViewController.excludedActivityTypes = excludedActivities
+        
+        
+        activityViewController.popoverPresentationController?.sourceView=self.view
+        
+        //If user on iPad
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            if activityViewController.responds(to: #selector(getter: UIViewController.popoverPresentationController)) {
+            }
+        }
+        present(activityViewController, animated: true, completion: nil)
+    }
 }
 
+extension UIView {
+    // Export pdf from Save pdf in drectory and return pdf file path
+    func exportAsPdfFromView() -> String {
+        
+        let pdfPageFrame = self.bounds
+        let pdfData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, pdfPageFrame, nil)
+        UIGraphicsBeginPDFPageWithInfo(pdfPageFrame, nil)
+        guard let pdfContext = UIGraphicsGetCurrentContext() else { return "" }
+        self.layer.render(in: pdfContext)
+        UIGraphicsEndPDFContext()
+        return self.saveViewPdf(data: pdfData)
+    }
+    
+    // Save pdf file in document directory
+    func saveViewPdf(data: NSMutableData) -> String {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docDirectoryPath = paths[0]
+        let pdfPath = docDirectoryPath.appendingPathComponent("viewPdf.pdf")
+        if data.write(to: pdfPath, atomically: true) {
+            return pdfPath.path
+        } else {
+            return ""
+        }
+    }
+}
